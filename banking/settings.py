4c4a8e8e5c2a18269fb2a5111cc945ee1cf1a779
Bharat Joshi <68678165+bharatjoshi3000@users.noreply.github.com>
2021-03-21 01:14:31 +0530

    Update settings.py

--- banking/settings.py
+++ banking/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'banking_system',               
         'USER': 'postgres',
-        'PASSWORD':'joshibh',      
+        'PASSWORD':'#',      
         'HOST': 'localhost',
     }
 }
