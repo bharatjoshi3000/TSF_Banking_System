7b2a67f4622d83990f3f0f2a6c9f30b9dc53996b
Bharat Joshi <68678165+bharatjoshi3000@users.noreply.github.com>
2021-03-21 12:20:05 +0530

    Commit

--- banking/urls.py
+++ banking/urls.py
@@ -17,8 +17,14 @@ from django.contrib import admin
 from django.urls import path ,include
 from transaction import views
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('',views.home,name= 'home'),
     path('', include('transaction.urls')),
+    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
